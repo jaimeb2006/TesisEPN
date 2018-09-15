73162981a06d5d415b4a39eac33cc5846c08a7ac
Jaimeb2006 <jaimeb_2006@hotmail.com>
2018-09-14 21:05:13 -0500

    Creacion de componentes de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    UserPageComponent,
+    AdminPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
