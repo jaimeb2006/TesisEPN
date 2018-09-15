acc19f130ce9639ea78e330bbe94571b9821f413
Jaimeb2006 <jaimeb_2006@hotmail.com>
2018-09-14 21:34:07 -0500

    Creacion del navbar y rutas inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+  { path: 'admin', component: AdminPageComponent},
+  { path: 'home', component: HomePageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'nofound', component: NotFoundPageComponent},
+  { path: 'register', component: RegisterPageComponent},
+  { path: 'user', component: UserPageComponent},
+  {path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
